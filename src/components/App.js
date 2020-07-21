8d2cf6cdb2daec8ba7c53b25eb3bb5bb8afec24d
Natalia Mateo <nmateo7@gmail.com>
2020-07-21 13:00:38 +0200

    Create and clean react app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Hola</p>
+    </div>
+  );
+}
+
+export default App;
