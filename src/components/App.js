57bbdea0e30e4a375542ec87c6475e94a2f496f5
Natalia Mateo <nmateo7@gmail.com>
2020-07-22 12:00:17 +0200

    Do kata2 html

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../stylesheets/App.scss';
 import Kata1 from './Kata1';
+import Kata2 from './Kata2';
 
 const App = () => {
   return (
     <div className='App'>
       <Kata1 />
+      <Kata2 />
     </div>
   );
 };
---
+++ src/components/Kata2.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState } from 'react';
+import '../stylesheets/CasingStrings.scss';
+import TextInput from './TextInput';
+import TextResult from './TextResult';
+
+const Pangram = () => {
+  let [text, setText] = useState('');
+
+  const changeText = (value) => {
+    setText(value);
+  };
+
+  const isAPangram = (text) => {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    let letters = text
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphabet.includes(character));
+
+    let letrasVistas = {};
+
+    for (const letter of alphabet) {
+      letrasVistas[letter] = 0;
+    }
+
+    for (const letter of letters) {
+      letrasVistas[letter] += 1;
+    }
+
+    for (const count of Object.values(letrasVistas)) {
+      if (count === 0) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  return (
+    <div className='kata2'>
+      <h2 className='kata2__title'>Detectando pangramas! </h2>
+      <p className='kata2__text'> Introduce la frase "The quick brown fox jumps over the lazy dog" y mira el resultado</p>
+      <div className='kata2__pangram'>
+        <TextInput onChangeText={changeText} />
+        <TextResult value={isAPangram(text)} />
+      </div>
+    </div>
+  );
+};
+
+export default Pangram;
