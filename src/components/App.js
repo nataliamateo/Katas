cdb79d468580e8b7c689f7560b8ce38216ded021
Natalia Mateo <nmateo7@gmail.com>
2020-07-22 13:32:58 +0200

    Create HTML and CSS kata4

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../stylesheets/App.scss';
 import Kata1 from './Kata1';
 import Kata2 from './Kata2';
 import Kata3 from './Kata3';
+import Kata4 from './Kata4';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Kata1 />
       <Kata2 />
       <Kata3 />
+      <Kata4 />
     </div>
   );
 };
---
+++ src/components/Kata4.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react';
+import '../stylesheets/Kata4.scss';
+import TextInput from './TextInput';
+import TextResult from './TextResult';
+
+const CamelCase = () => {
+  let [text, setText] = useState('');
+
+  const changeText = (value) => {
+    setText(value);
+  };
+
+  const toCamelCase = (text) => {
+    if (text.length === 0) {
+      return '';
+    }
+
+    let result = text
+      .replace(/\-|_/gi, ' ')
+      .split(' ')
+      .map((word) => word.toLowerCase())
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join('');
+
+    return text.charAt(0) + result.slice(1);
+  };
+
+  return (
+    <div className='kata4'>
+      <h2 className='kata4__title'>CamelCase </h2>
+      <p className='kata4__text'>Escribe una frase para ver el resultado de camelCase </p>
+      <div className='kata4__camelCase'>
+        <TextInput onChangeText={changeText} />
+        <TextResult value={toCamelCase(text)} />
+      </div>
+    </div>
+  );
+};
+
+export default CamelCase;
