70e2b553b8b30ab9aa59322d10af45ae63249986
Natalia Mateo <nmateo7@gmail.com>
2020-07-25 17:40:00 +0200

    Create Index

---
+++ src/components/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <ul>
+      <li>Capitalizar la primera letra de cada palabra</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+};
+
+export default Index;
