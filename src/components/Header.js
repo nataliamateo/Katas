1ce25e1735fc60607803254b3f1738a29e54d825
Natalia Mateo <nmateo7@gmail.com>
2020-07-25 19:59:57 +0200

    Do header html and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,54 @@ import '../stylesheets/Header.scss';
 const Header = () => {
   return (
     <header className='header'>
-      <nav>
-        <ul>
-          <li>Capitalizar la primera letra de cada palabra</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+      <nav className='header__nav'>
+        <h2 className='nav__title'>Índice</h2>
+        <ul className='nav__list'>
+          <li className='nav__list--item'>
+            <a href='/Kata1' className='url'>
+              Kata 1
+            </a>
+          </li>
+          <li>
+            <a href='/Kata2' className='url'>
+              Kata 2
+            </a>
+          </li>
+          <li>
+            <a href='/Kata3' className='url'>
+              Kata 3
+            </a>
+          </li>
+          <li>
+            <a href='/Kata4' className='url'>
+              Kata 4
+            </a>
+          </li>
+          <li>
+            <a href='/Kata5' className='url'>
+              Kata 5
+            </a>
+          </li>
+          <li>
+            <a href='/Kata6' className='url'>
+              Kata 6
+            </a>
+          </li>
+          <li>
+            <a href='/Kata7' className='url'>
+              Kata 7
+            </a>
+          </li>
+          <li>
+            <a href='/Kata8' className='url'>
+              Kata 8
+            </a>
+          </li>
+          <li>
+            <a href='/Kata9' className='url'>
+              Kata 9
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
